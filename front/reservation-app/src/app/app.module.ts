cb39660bbfe084b78262f54a0bfa9aa04dee929e
Nishijima Hideyuki <>
2023-10-29 10:43:58 +0900

    first commit

---
+++ front/reservation-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductModule } from './product/product.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front/reservation-app/src/app/product/products.ts
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    coverImage: './assets/img/placeholder.svg',
+    name: 'Phone 100',
+    price: 100,
+    description: 'A large phone with one of the best screens',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    heading1Text: 'サンプルテキスト1',
+    heading2Text: 'サンプルテキスト2',
+    heading3Text: 'サンプルテキスト3'
+  },
+  {
+    coverImage: './assets/img/placeholder.svg',
+    name: 'Phone 200',
+    price: 200,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    heading1Text: 'サンプルテキスト1',
+    heading2Text: 'サンプルテキスト2',
+    heading3Text: 'サンプルテキスト3'
+  },
+  {
+    coverImage: './assets/img/placeholder.svg',
+    name: 'Phone 300',
+    price: 300,
+    description: '',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    heading1Text: 'サンプルテキスト1',
+    heading2Text: 'サンプルテキスト2',
+    heading3Text: 'サンプルテキスト3'
+  },
+  {
+    coverImage: './assets/img/placeholder.svg',
+    name: 'Phone 400',
+    price: 400,
+    description: '',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    heading1Text: 'サンプルテキスト1',
+    heading2Text: 'サンプルテキスト2',
+    heading3Text: 'サンプルテキスト3'
+  }
+];
